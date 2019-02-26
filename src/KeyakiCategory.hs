1706615588b8d158f4a1e0cbab4abe25854482ce
T. N. Hayashi <net@hayashi-lin.net>
2019-02-26 17:38:24 +0900

    improve parser of KeyakiCategory

--- src/KeyakiCategory.hs
+++ src/KeyakiCategory.hs
@@ -13,7 +13,6 @@ import qualified Text.Parsec as Psc
 import Text.Parsec.String (Parser)
 
 import qualified PTPrintable as PTP
-import qualified StringWithBrackets as SWB
 
 data KeyakiCategory =
     KeyakiCategory {
@@ -33,7 +32,16 @@ createBase strlist
 
 printCatList :: KeyakiCategory -> String
 printCatList (KeyakiCategory catList _ _)
-    = DL.intercalate "-" catList
+    = DL.intercalate "-" $ escapedCat <$> catList
+    where 
+        isToBeEscaped :: String -> Bool
+        isToBeEscaped
+            = any (\c -> DC.isSpace c || c `elem` "()-|")
+        escapedCat :: String -> String
+        escapedCat str
+            = if isToBeEscaped str
+                then '{' : (str ++ "}")
+                else str -- '\"' : (str ++ "\"")
 
 instance Show KeyakiCategory where
     show all@(KeyakiCategory _ iched sortInfo)
@@ -42,30 +50,67 @@ instance Show KeyakiCategory where
             ++ (";{" ++ sortInfo ++ "}")
 
 instance PTP.Printable KeyakiCategory where
-    psdPrint (PTP.Option _ PTP.Minimal) = printCatList
-    psdPrint _ = show
-    
+    psdPrint (PTP.Option _ PTP.Minimal) 
+        = printCatList
+    psdPrint _ 
+        = show
+
+parserBracketedString :: Parser String
+parserBracketedString
+    = (
+        Psc.between (Psc.char '{') (Psc.char '}') 
+        $ Psc.many (Psc.noneOf "}")
+    ) Psc.<?> "Bracketed String"
+
+
+parserString :: Parser String
+parserString
+    = (Psc.many1 parserNonEscapeChars)
+        Psc.<?> "Non-Bracketed String"
+    where 
+        condition :: Char -> Bool
+        condition c
+            = not $ DC.isSpace c || c `elem` ";{}()-|"
+        parserNonEscapeChars :: Parser Char
+        parserNonEscapeChars
+            = Psc.satisfy condition
+
+parserSemicolon :: Parser String
+parserSemicolon
+    = (Psc.notFollowedBy $ Psc.string ";{")
+        *> Psc.string ";" Psc.<?> "Semicolon"
+
 parser :: Parser KeyakiCategory
 parser 
-    = do
-        catICH <- concat <$> 
-            (Psc.many1 
-            $ SWB.parserStringOrBracketedString "()|"); -- String
-        -- TODO: 普通の;と;{...}との区別。先読みが必要。
-        sort <- Psc.option "" (
-            Psc.try $
-            Psc.between (Psc.string ";{") (Psc.char '}')
-                (Psc.many (Psc.noneOf "}"))
-            );
-        return $ (makeKC $ splitCatICH catICH) { sortInfo = sort }
+    =  parserCatICH
+        >>= \catICH ->
+            Psc.option "" (
+                Psc.between (Psc.string ";{") (Psc.char '}')
+                    $ Psc.many (Psc.noneOf "}")
+            )
+            >>= \sort ->
+                return $ catICH { sortInfo = sort }
     where
-        splitCatICH :: String -> [String]
-        splitCatICH = DLS.splitOn "-" 
+        parserStringOrBracketedString :: Parser String
+        parserStringOrBracketedString
+            = Psc.many1 (
+                parserSemicolon 
+                Psc.<|> parserBracketedString 
+                Psc.<|> parserString
+                )
+                >>= (return . concat)
         makeKC :: [String] -> KeyakiCategory
         makeKC li
             | length li > 1 && all DC.isDigit (last li) 
-                = (createBase (init li)) {iched = last li}
-            | otherwise = createBase li
+                = (createBase (init li)) { iched = last li }
+            | otherwise 
+                = createBase li
+        parserCatICH :: Parser KeyakiCategory
+        parserCatICH
+            = makeKC 
+                <$> (
+                    parserStringOrBracketedString `Psc.sepBy1` (Psc.char '-')
+                )
 
 -- | generate an Keyaki-Category from a string or a stream. 
 createFromString :: String -> Either Psc.ParseError KeyakiCategory
