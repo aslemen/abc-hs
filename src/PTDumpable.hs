4742e70715fda3c7e1908beb1337373693db43bb
T. N. Hayashi <net@hayashi-lin.net>
2019-05-08 16:01:41 +0900

    add documentation

--- src/PTDumpable.hs
+++ src/PTDumpable.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE FlexibleInstances #-}
 
+{-|
+    Module:     PTDumpable
+    Copyright:  (c) T. N. Hayashi, 2019
+    License:    Undetermined
+
+    Provide an type class type of which 
+    supports pretty serialization in the munge-trees style, 
+    serving as an extension of the Show class in Prelude.
+-}
 module PTDumpable (
+    -- * Types
     TreeStyle(..),
     NodeStyle(..),
     Option(..),
+    -- * Classes
     Dumpable(..)
     ) where
 
@@ -12,29 +23,34 @@ import qualified Data.Text as DT
 import qualified Data.Text.Lazy as DTL
 import qualified Data.Text.Lazy.Builder as DTLB
 
-{-- 
-    ======
-    Data Types
-    ======
---}
+-- | 'TreeStyle' specifies the serialization style of trees.
 data TreeStyle 
-    = OneLine | Pretty 
-        deriving (Eq, Show)
+    = OneLine -- ^ Each tree is serialized in one line.
+    | Pretty  -- ^ Each tree is serialized prettily in the same way of munge-trees.
+    deriving (Eq, Show)
+
+-- | 'NodeStyle' speficies the serialized style of node labels.
 data NodeStyle 
-    = Minimal | Default | Full
-        deriving (Eq, Show)
+    = Minimal -- ^ Each node is serialized as short as is possible.
+    | Default -- ^ Each node is serialized normally.
+    | Full    -- ^ Each node is serialized with full information.
+    deriving (Eq, Show)
+
+-- | 'Option' bundles all the serialization options mentioned above.
 data Option 
     = Option { 
+        -- | The serialization style of tree structures.
         treeStyle :: TreeStyle, 
+        -- | The serialization style of nodes.
         nodeStyle :: NodeStyle
         } deriving (Eq, Show)
 
-{--
-    ======
-    CLass Definition
-    ======
---}
+-- | 'Dumpable' is a class of types which support the
+--      munge-trees style serialization of trees.
+--      A tree is serialized with 'Data.Text.Lazy.Builder',
+--          which needs further conversion to Data.Text or others.
 class (Show a) => Dumpable a where
+    -- | Serialize a tree with options.
     psdDump :: Option -> a -> DTLB.Builder
     psdDump (Option Pretty Default)  -- 普段遣い
         = DTLB.fromString . show 
@@ -46,11 +62,14 @@ class (Show a) => Dumpable a where
         = DTLB.fromLazyText
             . (DTL.filter (\c -> c /= '\n')) 
             . DTLB.toLazyText 
-            . psdDump (Option Pretty node) -- defaultではやはりshow
+            . psdDump (Option Pretty node)
+    -- | Serialized a tree with the default option.
+    --
+    --      * 'PTDumpable.treeStyle': 'PTDumpable.Pretty'
+    --      * 'PTDumpable.nodeStyle': 'PTDumpable.Default'
     psdDumpDefault :: a -> DTLB.Builder
     psdDumpDefault 
-        = psdDump (Option Pretty Default) 
-        -- default print style
+        = psdDump (Option Pretty Default)
 
 instance Dumpable String
 
